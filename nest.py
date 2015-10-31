5c64398c99a5a589b3b4030066b6534679b1b82b
acraddoc91 <acraddoc@umd.edu>
2015-10-30 23:38:46 -0400

    Changed stuff around so that all user details are in AuthDetails file

--- nest.py
+++ nest.py
@@ -1,11 +1,15 @@
 import requests
 import json
 
-#This is the authorisation code for your nest account generated when you set up through REST
-authcode = ''
+#This reads you're authirsation code and thermostat ID from the AuthDetails file. You'll need to change these for you
+f = open('AuthDetails', 'r')
+firstline = f.readline()
+secondline = f.readline()
+authcode = firstline[9:len(firstline)-1]
+thermID = secondline[8:]
+f.close()
+
 baseurl = 'https://developer-api.nest.com/devices/thermostats/'
-#This is the ID for your particular thermostat you want to interface with. You'll probably want to change this.
-thermID = ''
 totURL = baseurl + thermID + '?auth=' + authcode
 
 def getData():
