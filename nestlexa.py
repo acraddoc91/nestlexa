69c5e3fa0bb33845781105f48d93da03e462df06
acraddoc91 <acraddoc@umd.edu>
2015-11-21 19:07:24 -0500

    Added cards for various responses

--- nestlexa.py
+++ nestlexa.py
@@ -7,11 +7,13 @@ sys.path += [os.path.dirname(__file__)]
 import nest
 
 #take any speech to be sent to alexa and convert it to json form it wants
-def constructResponse(speech = None, end_session = True):
+def constructResponse(speech = None, card = None, end_session = True):
 	reply = {"version" : "1.0"}
 	response = {}
 	if speech:
 		response['outputSpeech'] = {'type':'PlainText', 'text':speech}
+	if card:
+		response['card'] = {'type':'Simple', 'title':'Nestlexa', 'content':card}
 	response['shouldEndSession'] = end_session
 	reply['response'] = response
 	return json.dumps(reply)
@@ -19,39 +21,48 @@ def constructResponse(speech = None, end_session = True):
 def setAbsTemp(temp):
 	if temp > 78:
 		speech = 'That\'s pretty hot, if you really want it above 78 set it manually'
+		card = None
 	elif temp < 64:
 		speech = 'That\'s pretty cold, if you really want it below 64 set it manually'
+		card = None
 	else:
 		payload = {'target_temperature_f':temp}
 		r = nest.putData(payload)
 		if r == 200:
 			speech = 'The target temperature has been set to ' + str(temp) + ' degrees farenheit'
+			card = 'The target temperature has been set to ' + str(temp) + ' degrees farenheit'
 		else:
 			speech = 'Something appears to have gone wrong, try again'
-	return speech
+			card = None
+	return {'speech':speech, 'card':card}
 	
 def doIntent(intentJson):
 	if intentJson['name']=='GetTemp':
 		data = nest.getData()
 		speech = 'The current temperature in the apartment is ' + str(data['ambient_temperature_f']) + ' degrees farenheit'
-		return constructResponse(speech)
+		card = 'The current temperature in the apartment is ' + str(data['ambient_temperature_f']) + ' degrees farenheit'
+		return constructResponse(speech, card)
 	if intentJson['name']=='SetTemp':
 		temp = int(intentJson['slots']['temp']['value'])
-		return constructResponse(setAbsTemp(temp))
+		response = setAbsTemp(temp)
+		return constructResponse(response['speech'],response['card'])
 	if intentJson['name']=='BumpTemp':
 		data = nest.getData()
 		currTemp = int(data['target_temperature_f'])
 		newTemp = currTemp + int(intentJson['slots']['bump']['value'])
-		return constructResponse(setAbsTemp(newTemp))
+		response = setAbsTemp(temp)
+		return constructResponse(response['speech'],response['card'])
 	if intentJson['name']=='DropTemp':
 		data = nest.getData()
 		currTemp = int(data['target_temperature_f'])
 		newTemp = currTemp - int(intentJson['slots']['drop']['value'])
-		return constructResponse(setAbsTemp(newTemp))
+		response = setAbsTemp(temp)
+		return constructResponse(response['speech'],response['card'])
 	if intentJson['name']=='GetSetTemp':
 		data = nest.getData()
 		speech = 'The current set temperature is ' + str(data['target_temperature_f']) + ' degrees farenheit'
-		return constructResponse(speech)
+		card = 'The current set temperature is ' + str(data['target_temperature_f']) + ' degrees farenheit'
+		return constructResponse(speech, card)
 	else:
 		speech = 'I\'m not sure I can do that, ask me something else'
 		return constructResponse(speech)
